6ac0d9690a552baa8bab117fb3ea2eb1b76aa4ef
Mohamed Shadab <shadabdragon@gmail.com>
2018-03-12 01:48:19 +0530

    Windows Run

--- index.js
+++ index.js
@@ -1,31 +1,41 @@
-var punify = function() {
-  var twit = require('twit');
-  var config = require('./config.js'); // import config file containing keys and tokens
+var sleep = require('system-sleep');
 
-  var twitter = new twit(config);
-  var content = 'Adventure. Excitement. A Jedi craves not these things.';
-  twitter.get('search/tweets', {
-    q: 'adventure OR excitement -filter:retweets',
-    count: 1,
-    result_type: 'recent',
-    tweet_mode: 'extended',
-    lang: 'en'
-  }).catch(function(err) {
-    console.log('Caught Error', err.stack);
-  }).then(function(result) {
-    var tweet = result.data.statuses[0];
-    var tweetID = tweet.id_str;
-    var username = tweet.user.screen_name;
-    twitter.post('statuses/update', {
-      status: '@' + username + ' ' + content,
-      in_reply_to_status_id: tweetID
-    }, function(err, data, response) {
-      if (err) {
+function fire() {
+  while (1) {
+    var punify = function () {
+      var twit = require('twit');
+      var config = require('./config.js'); // import config file containing keys and tokens
+
+      var twitter = new twit(config);
+      var content = 'Darkness my old friend.';
+      twitter.get('search/tweets', {
+        q: 'darkness -filter:retweets',
+        count: 1,
+        result_type: 'recent',
+        tweet_mode: 'extended',
+        lang: 'en'
+      }).catch(function (err) {
         console.log('Caught Error', err.stack);
-      }
-      //console.log(data);
-    })
-  });
+      }).then(function (result) {
+        var tweet = result.data.statuses[0];
+        var tweetID = tweet.id_str;
+        var username = tweet.user.screen_name;
+        twitter.post('statuses/update', {
+          status: '@' + username + ' ' + content,
+          in_reply_to_status_id: tweetID
+        }, function (err, data, response) {
+          if (err) {
+            console.log('Caught Error', err.stack);
+          }
+        })
+      });
+    }
+    punify(); // call function to punify the tweet
+
+    console.log('tweet');
+    //3 minutes of sleep before next run (tweet) 
+    sleep(180000); //1000 = 1 second, change accordingly
+  }
 }
 
-punify(); // call function to punify the tweet
+fire();
